b9fc778c8ad6b16f51b9470b1c9ede2b3579d51e
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-01-16 15:40:14 +0530

    Update VariableJS.js

---
+++ Variables/VariableJS.js
@@ -0,0 +1,10 @@
+var a=5;  //right to left associativity
+var b=4;
+var x="Charuni";
+var y="Rathnayaka";
+
+var sum=a+b; //left to right associativity
+var name=x+" "+y;
+var d=x+b+a;
+var e=sum+a*b;
+document.write(sum);
