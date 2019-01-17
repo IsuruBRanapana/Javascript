57fffa86d222ca335fc166ee320b442fa85ae4ab
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-01-17 21:40:12 +0530

    If Statement

---
+++ Control Statement/Control StatementsJS.js
@@ -0,0 +1,30 @@
+var a=6;
+if(a%2==0)
+{
+    document.write("Number is EVEN");
+}
+else
+{
+    document.write("Number is ODD");
+}
+
+var b=7;
+if(b>0)
+{
+    document.write("Number is possitive");
+}
+else if(b<0)
+{
+    document.write("Number is negative");
+}
+else
+{
+    document.write("Nuber can be negative or possitive");
+}
+
+
+var c=8;
+if(c>0 && c%2==0)
+{
+    document.write("Number is possitive and Even");
+}
