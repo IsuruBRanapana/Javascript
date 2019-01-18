7568273f77d79d8badda25747d8c092f6b0e20f5
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-01-18 22:15:29 +0530

    learn Switch case

---
+++ Control Statement/Switch.js
@@ -0,0 +1,27 @@
+var x=15;
+switch(x)
+{
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("Wednesday");
+        break;
+    case 4:
+        document.write("Thursday");
+        break;
+    case 5:
+        document.write("Friday");
+        break;
+    case 6:
+        document.write("Saterday");
+        break;
+    case 7:
+        document.write("Sunday");
+        break;
+    default:
+        document.write("Wrong input");
+}
